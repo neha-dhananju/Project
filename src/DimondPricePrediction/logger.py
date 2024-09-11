c9dbdda8c7b343c9b9bc537a5a6da6c6168fdb4b
neha-dhananju <neha02dhananju@gmail.com>
2024-09-11 12:28:20 +0530

    updated

---
+++ src/DimondPricePrediction/logger.py
@@ -0,0 +1,12 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+log_path=os.path.join(os.getcwd(),"logs")
+
+os.mkdir(log_path,exist_ok=True)
+
+LOG_FILEPATH=os.path.join(log_path,LOG_FILE )
+  
