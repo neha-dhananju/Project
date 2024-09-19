16e4266f1257f96fe6fe8850679b55f3e93eba30
neha-dhananju <neha02dhananju@gmail.com>
2024-09-19 12:12:00 +0530

    updated

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -5,3 +5,8 @@ import sys
 from src.DimondPricePrediction.logger import logging
 from src.DimondPricePrediction.exception import customexception
 import pandas as pd
+
+
+obj=DataIngestion()
+
+obj.initiate_data_ingestion()
